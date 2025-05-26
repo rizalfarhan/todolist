cf1f20dfd988c9c8d97e8b5c1ace6137e78da210
Rizal Farhan <rizalfarhannanda@gmail.com>
2025-05-26 19:27:59 +0700

    deskripsi

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'To Do List',
+  description: 'to do list courses and tasks',
   generator: 'v0.dev',
 }
 
