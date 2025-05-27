7fdf2c4eed9ec88b0af8566998348d49bcb761de
Rizal Farhan <rizalfarhannanda@gmail.com>
2025-05-27 21:58:28 +0700

    ganti nama

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'To Do List',
+  title: 'StudyMate',
   description: 'to do list courses and tasks',
   generator: 'v0.dev',
 }
